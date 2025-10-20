2f4d9d199601225097aee6eed880c84c8c659771
Jose Vera <jose.vera@nazaries.com>
2025-10-20 14:22:44 +0200

    added navigation to list-users

--- spa/src/app/modules/landing/dashboard-page/dashboard-page.component.ts
+++ spa/src/app/modules/landing/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AuthService } from '@app/core/auth/services/auth.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
