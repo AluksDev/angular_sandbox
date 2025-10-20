16757d9062257ace93c3c2767fd694eb81d94644
Jose Vera <jose.vera@nazaries.com>
2025-10-20 14:16:39 +0200

    userResponse defined

--- spa/src/api/defs/User.ts
+++ spa/src/api/defs/User.ts
@@ -1,11 +1,22 @@
 /* tslint:disable:max-line-length */
 
+export interface UserResponse {
+  count:    number;
+  total:    number;
+  next:     null;
+  previous: null;
+  results:  User[];
+}
+
+
 export interface User {
   id?: number;
   /** format: email */
   email?: string;
   dni?: string;
   username: string;
+  first_name: string;
+  last_name:  string;
   /** format: date-time */
   date_joined?: string;
   /** format: date-time */
