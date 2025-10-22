d039421089a3414c0e2aa23062c023a2e8ca6af3
Jose Vera <jose.vera@nazaries.com>
2025-10-22 14:08:49 +0200

    user card added

--- spa/src/app/modules/landing/profile.component/profile.component.ts
+++ spa/src/app/modules/landing/profile.component/profile.component.ts
@@ -1,9 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+
+
+import { ProfileCardComponent } from "./components/profile-card.component/profile-card.component";
+import { UserService } from '../user.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+
 
 @Component({
   selector: 'profile-page',
-  imports: [],
+  imports: [ProfileCardComponent, MatProgressSpinnerModule],
   templateUrl: './profile.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileComponent { }
+export class ProfileComponent { 
+
+  userService = inject(UserService)
+
+
+  userResource = rxResource({
+    loader: ({}) => {
+      return this.userService.get();
+    }
+  })
+
+}
