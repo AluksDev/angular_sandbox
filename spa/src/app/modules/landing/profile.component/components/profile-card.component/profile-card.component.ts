35416582bc54f9ae389d8f7a2ec7fe41df69c7fa
Jose Vera <jose.vera@nazaries.com>
2025-10-22 14:08:15 +0200

    create user card component

--- spa/src/app/modules/landing/profile.component/components/profile-card.component/profile-card.component.ts
+++ spa/src/app/modules/landing/profile.component/components/profile-card.component/profile-card.component.ts
@@ -1,9 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+
+import { User } from '@api/defs/User';
+import { MatDivider } from "@angular/material/divider";
+import { DatePipe, TitleCasePipe } from '@angular/common';
+import { AvatarComponent } from "../avatar.component/avatar.component";
+// import { User } from '@app/core/user/user.types';
 
 @Component({
   selector: 'profile-card',
-  imports: [],
+  imports: [MatCardModule, MatIcon, MatDivider, DatePipe, TitleCasePipe, AvatarComponent],
   templateUrl: './profile-card.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './profile-card.component.css',
 })
-export class ProfileCardComponent { }
+export class ProfileCardComponent { 
+
+  user = input.required<User>();
+
+}
