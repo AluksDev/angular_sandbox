c004cb53ad1c7d38a9a9b56997ff8a62751c3655
Jose Vera <jose.vera@nazaries.com>
2025-10-27 12:34:07 +0100

    implemented card component

--- spa/src/app/modules/departments/department-card.component/department-card.component.ts
+++ spa/src/app/modules/departments/department-card.component/department-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { Department } from '@api/defs/Department';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-department-card',
-  imports: [],
+  selector: 'department-card',
+  imports: [MatCardModule, MatIcon],
   templateUrl: './department-card.component.html',
+  styleUrl: './department-card.component.css'
 })
 export class DepartmentCardComponent { 
 
+  department = input.required<Department>()
 }
