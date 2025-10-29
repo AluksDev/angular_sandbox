5b829ec90d525e2c038baf219a6f5d22c520c783
Jose Vera <jose.vera@nazaries.com>
2025-10-29 08:56:42 +0100

    Add create, edit, and delete functionality

--- spa/src/app/modules/departments/department.service.ts
+++ spa/src/app/modules/departments/department.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Department, DepartmentResponse } from '@api/defs/Department';
+import { Router } from '@angular/router';
+import { Department, DepartmentCreate, DepartmentResponse, DepartmentUpdate } from '@api/defs/Department';
+import { NotificationService } from '@utils/services/notification.service';
 import { environment } from 'environments/environment.hmr';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 
 const baseUrl = environment.apiUrl;
 
@@ -12,6 +14,8 @@ const baseUrl = environment.apiUrl;
 export class DepartmentService {
 
   private _httpClient = inject(HttpClient);
+  private _notificationService = inject(NotificationService);
+  private router = inject(Router);
   
 
   /**
@@ -40,6 +44,89 @@ export class DepartmentService {
         results: mappedResults
       }
     }));
+  }
+
+   /**
+    * Create a new department
+    * @param department the data object
+    * @returns An Observable that emits the newly created department object
+    * 
+    * @throws {Error} If the department name already exists or if there's a database error.
+    */
+  create(department: DepartmentCreate): Observable<Department>{
+
+    console.log(department);
+    
+    const { name , code = ''} = department;
+
+    return this._httpClient.post<Department>(`${baseUrl}/department/`, {
+      name: name,
+      code: code
+    }).pipe(
+      catchError((error:any) =>{
+        const transformedError = this.handleError(error);
+        
+        return throwError(() => transformedError);
+      })
+    );
+
+  }
+
+  /**
+   * Update a department
+   * @param id the unique id of the department
+   * @param department the filds to update
+   * @returns An Observable that emits the updated department object
+   */
+  update(id: number, department: DepartmentUpdate): Observable<Department>{
+
+    const {name, code} = department;
+
+    return this._httpClient.patch<Department>(`${baseUrl}/department/${id}/`, {
+      name: name,
+      code: code
+    }).pipe(
+      tap(()=> {
+        const currentUrl = this.router.url;
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+            this.router.navigateByUrl(currentUrl);
+        });
+        this._notificationService.showSuccess('Departamento actualizado exitosamente');
+      }),
+      catchError((error:any) =>{
+        const transformedError = this.handleError(error);
+        
+        return throwError(() => transformedError);
+      })
+    );
+
+  } 
+
+  /**
+   * Delete a department from the system using its unique ID.
+   * @param id the unique id of the department
+   * @returns 
+   */
+  delete(id: number): Observable<void>{
+    return this._httpClient.delete<void>(`${baseUrl}/department/${id}`);
+  }
+
+  /**
+   * Handles HTTP errors received from the API and transforms them into a consistent
+   * application-specific error format (AppError).
+   *
+   * @private
+   * @param error - The raw error
+   * @returns An object with custom status and message
+   */
+  private handleError(error: any){
+
+    if(error.status == 400){
+      return {
+        statusCode: 500,
+        message: 'Ya existe departamento con ese nombre'
+    };
+    }
   }
 
   /**
