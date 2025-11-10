b21a073a1458d1cbfa44967407e57cd5665990e0
Jose Vera <jose.vera@nazaries.com>
2025-11-10 14:08:51 +0100

    removed unused imports

--- spa/src/app/modules/landing/dashboard-page/dashboard-page.component.ts
+++ spa/src/app/modules/landing/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AuthService } from '@app/core/auth/services/auth.service';
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
