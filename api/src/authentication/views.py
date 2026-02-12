1af81c29dd2de85c9479960da39261f516679b69
Jose Miguel <jmdiazhervas@gmail.com>
2026-02-12 11:43:28 +0100

    fix view logout

--- api/src/authentication/views.py
+++ api/src/authentication/views.py
@@ -46,7 +46,7 @@ class AuthView(viewsets.GenericViewSet):
         }, status=status.HTTP_200_OK)
 
     @swagger_auto_schema(
-        request_body={},
+        request_body=None,
         responses={200: 'Logged out successfully'}
     )
     @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
