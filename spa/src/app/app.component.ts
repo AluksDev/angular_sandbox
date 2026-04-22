718200143ad1464bb00992d29c8bc5a5023c8046
alberto lucchetti <alberto.lucchetti@nazaries.com>
2026-04-22 11:19:36 +0000

    Changed layout and improved visuals

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -1,100 +1,11 @@
-import { NgClass } from "@angular/common";
-import { Component, inject, OnDestroy, OnInit } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
-import { AuthService } from "@app/core/auth/auth.service";
-import { FuseNavigationItem, FuseNavigationService } from "@fuse/components/navigation";
-import { FuseConfigService } from "@fuse/services/config";
-import { FuseThemeService } from "@fuse/services/theme/theme.service";
-import { NasiaLayoutComponent } from "@layout/layouts/nasia-layout/nasia-layout.component";
-import { NgSelectConfig } from "@ng-select/ng-select";
-import { Subject, Subscription } from "rxjs";
-
-import { Router } from "@angular/router";
-import { LayoutService } from "@utils/layout.service";
-import { defaultNavigation } from "./core/navigation/navigationMap";
-import { RuntimeConfigService } from "./runtime-config.service";
+import { Component } from "@angular/core";
+import { MainLayout } from "./layouts/main-layout/main-layout";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
-  imports: [NasiaLayoutComponent, NgClass],
+  imports: [MainLayout],
   providers: [],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  mainColor = "theme_original";
-  private themeSubscription: Subscription;
-
-  private _bcnConfigService = inject(FuseConfigService);
-  private _bcnNavigationService = inject(FuseNavigationService);
-  private config = inject(NgSelectConfig);
-  themeService = inject(FuseThemeService);
-  private dialog = inject(MatDialog);
-  private authService = inject(AuthService);
-  private runtime = inject(RuntimeConfigService);
-  private layoutService = inject(LayoutService);
-  private _router = inject(Router);
-
-  navigation: FuseNavigationItem[];
-  bcnConfig: any;
-
-  // Private
-  private _unsubscribeAll: Subject<void>;
-
-  /** Inserted by Angular inject() migration for backwards compatibility */
-  constructor(...args: unknown[]);
-
-  /**
-   * Constructor
-   *
-   */
-  constructor(protected themeService2: FuseThemeService) {
-    // Get default navigation
-    this.navigation = defaultNavigation;
-
-    // Register the navigation to the service
-    this._bcnNavigationService.register("main", this.navigation);
-
-    // Set the main navigation as our current navigation
-    this._bcnNavigationService.setCurrentNavigation("main");
-
-    // Set the private defaults
-    this._unsubscribeAll = new Subject();
-
-    // Configure default ng-select
-    this.config.notFoundText = "Custom not found";
-    this.config.placeholder = "Seleccioneu l'element";
-    this.config.notFoundText = "No hi ha elements";
-    this.config.addTagText = "Afegir element";
-    this.config.typeToSearchText = "Cerca...";
-    this.config.loadingText = "Carregant...";
-    this.config.clearAllText = "Esborrar";
-
-    this.themeSubscription = this.themeService.currentTheme$.subscribe((theme) => {
-      this.mainColor = theme;
-    });
-  }
-
-  // -----------------------------------------------------------------------------------------------------
-  // @ Lifecycle hooks
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * On init
-   */
-  ngOnInit(): void {}
-
-  /**
-   * On destroy
-   */
-  ngOnDestroy(): void {
-    // Unsubscribe from all subscriptions
-    this._unsubscribeAll.next(null);
-    this._unsubscribeAll.complete();
-    this.themeSubscription.unsubscribe(); // Evitar memory leaks
-  }
-
-  // -----------------------------------------------------------------------------------------------------
-  // @ Public methods
-  // -----------------------------------------------------------------------------------------------------
-}
+export class AppComponent {}
---
+++ spa/src/app/layouts/main-layout/main-layout.ts
@@ -0,0 +1,71 @@
+import {Component, inject, signal} from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { Router, RouterOutlet } from '@angular/router';
+import { FuseNavigationItem, FuseVerticalNavigationComponent } from '@fuse/components/navigation';
+import { CdkTableModule } from "@angular/cdk/table";
+import { UserSidebarSnippetComponent } from "@app/core/user/components/user-sidebar-snippet-component/user-sidebar-snippet-component";
+import { AuthService } from '@app/core/auth/auth.service';
+import { finalize } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AsyncPipe, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [MatSidenavModule, FuseVerticalNavigationComponent, RouterOutlet, CdkTableModule, UserSidebarSnippetComponent, AsyncPipe, NgIf],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.scss',
+})
+export class MainLayout {
+  navigationItems: FuseNavigationItem[] = [
+    {
+      type: 'basic',
+      id: 'users',
+      title: 'Users',
+      link: '/users',
+      icon: 'mat_outline:group'
+    },
+    {
+      type: 'basic',
+      id: 'departments',
+      title: 'Departments',
+      link: '/departments',
+      icon: 'mat_outline:apartment'
+    }
+  ]
+
+  authService = inject(AuthService);
+  user$ = this.authService.currentUser$;
+  router = inject(Router);
+  loading = signal<boolean>(false);
+  _snackBar = inject(MatSnackBar);
+
+  onUserAction(action: string){
+    switch(action){
+      case 'logout':
+        this.logout();
+    }
+  }
+
+   logout(){
+    this.loading.set(true);
+    this.authService.logout()
+    .pipe(
+      finalize(()=> this.loading.set(false))
+    )
+    .subscribe({
+      next: ((res) => {
+        this.openSnackBar(res.detail, 'Close');
+        this.router.navigate(['/auth/login']);
+      }),
+      error: ()=>{
+        this.openSnackBar('Something went wrong', 'Close');
+      }
+    })
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 3000
+    });
+  }
+ }
