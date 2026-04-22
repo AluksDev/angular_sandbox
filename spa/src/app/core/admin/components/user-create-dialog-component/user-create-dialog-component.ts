1710c2feb6980b0ab8a07e5df85b0d9002c97910
AlucksDev <alberto.lucchetti@nazaries.com>
2026-04-22 13:15:17 +0200

    Initial styling of password generator

--- spa/src/app/core/admin/components/user-create-dialog-component/user-create-dialog-component.ts
+++ spa/src/app/core/admin/components/user-create-dialog-component/user-create-dialog-component.ts
@@ -3,10 +3,11 @@ import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormInputComponent } from "@app/shared/forms/components/form-input-component/form-input-component";
+import { PasswordGeneratorComponent } from "@app/shared/components/password-generator-component/password-generator-component";
 
 @Component({
   selector: 'app-user-create-dialog-component',
-  imports: [MatDialogModule, FormInputComponent, ReactiveFormsModule, FormsModule, MatButtonModule],
+  imports: [MatDialogModule, FormInputComponent, ReactiveFormsModule, FormsModule, MatButtonModule, PasswordGeneratorComponent],
   templateUrl: './user-create-dialog-component.html',
   styleUrl: './user-create-dialog-component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- spa/src/app/shared/components/password-generator-component/password-generator-component.ts
+++ spa/src/app/shared/components/password-generator-component/password-generator-component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-password-generator-component',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './password-generator-component.html',
   styleUrl: './password-generator-component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
