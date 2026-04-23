26649004e896897e504e1725741dc7b15fe31fa9
AlucksDev <alberto.lucchetti@nazaries.com>
2026-04-23 11:31:17 +0200

    Fixed custom select component. Added department and role selection in form

--- spa/src/app/features/admin/components/user-create-dialog-component/user-create-dialog-component.ts
+++ spa/src/app/features/admin/components/user-create-dialog-component/user-create-dialog-component.ts
@@ -4,15 +4,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormInputComponent } from "@app/shared/forms/components/form-input-component/form-input-component";
 import { PasswordGeneratorComponent } from "@app/shared/components/password-generator-component/password-generator-component";
+import { DepartmentsService } from '@app/features/departments/departments.service';
+import { FormSelectComponent } from "@app/shared/forms/components/form-select-component/form-select-component";
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-user-create-dialog-component',
-  imports: [MatDialogModule, FormInputComponent, ReactiveFormsModule, FormsModule, MatButtonModule, PasswordGeneratorComponent],
+  imports: [MatDialogModule, FormInputComponent, ReactiveFormsModule, FormsModule, MatButtonModule, PasswordGeneratorComponent, FormSelectComponent, AsyncPipe, JsonPipe],
   templateUrl: './user-create-dialog-component.html',
   styleUrl: './user-create-dialog-component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserCreateDialogComponent { 
+  departmentService = inject(DepartmentsService);
   fb = inject(FormBuilder);
   createUser = this.fb.group({
     username: [''],
@@ -26,5 +31,18 @@ export class UserCreateDialogComponent {
 
   passwordPattern =
   '^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{8,}$';
+
+  departments$ = this.departmentService.getDepartments().pipe(
+    map(result => {
+      return result.results.map( dep => {
+        return {
+          label: dep.name,
+          value: dep.id
+        }
+      })
+    })
+  );
+
+
 
 }
