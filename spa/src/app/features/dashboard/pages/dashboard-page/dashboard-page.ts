85ca97e0c5e9424df5d61d962f0f7d42ce5f9122
AlucksDev <alberto.lucchetti@nazaries.com>
2026-04-23 09:06:11 +0200

    Hide admin action panel in dashboard if user is not admin

--- spa/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
+++ spa/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
@@ -1,17 +1,30 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { UserCreateDialogComponent } from '@app/features/admin/components/user-create-dialog-component/user-create-dialog-component';
-import { Router, RouterLink } from "@angular/router";
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from "@angular/router";
+import { AuthService } from '@app/core/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterLink],
+  imports: [MatButtonModule],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardPage {
   router = inject(Router);
+  authService = inject(AuthService);
+  user = toSignal(this.authService.currentUser$);
+
+  isAdmin = computed(() => {
+    const user = this.user();
+    return !!user && (
+      user.roles.includes('admin') ||
+      user.roles.includes('superuser')
+    );
+  });
+
   openCreateUserDialog() {
     this.router.navigate(['/admin/users/create']);
   }
--- spa/src/app/shared/components/password-generator-component/password-generator-component.ts
+++ spa/src/app/shared/components/password-generator-component/password-generator-component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
@@ -8,4 +8,46 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './password-generator-component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PasswordGeneratorComponent { }
+export class PasswordGeneratorComponent {
+  generatedPassword = signal<string>('Password generator');
+
+  generatePassword() {
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()-_=+[]{};:,.?";
+    const all = lower + upper + numbers + symbols;
+    const totalLength = 12;
+
+    //Ensure one of each type
+    let password: string[] = [
+      lower[Math.floor(Math.random() * lower.length)],
+      upper[Math.floor(Math.random() * upper.length)],
+      numbers[Math.floor(Math.random() * numbers.length)],
+      symbols[Math.floor(Math.random() * symbols.length)]
+    ];
+
+    for (let i = password.length; i < totalLength; i++) {
+      password.push(all[Math.floor(Math.random() * all.length)]);
+    }
+
+    this.generatedPassword.set(this.shufflePassword(password).join(""));
+  }
+
+  //Fisher–Yates shuffle algorithm
+  shufflePassword(array: string[]): string[] {
+    const copy = [...array];
+
+    for (let i = copy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copy[i], copy[j]] = [copy[j], copy[i]];
+    }
+
+    return copy;
+  }
+
+  copyPswToClipboard() {
+    if (this.generatedPassword() === '') return null;
+    navigator.clipboard.writeText(this.generatedPassword());
+  }
+ }
