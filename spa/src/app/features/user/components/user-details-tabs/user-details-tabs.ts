ed25a1c03dbbcb5ddb077cf187ec46f0adcd5c4c
AlucksDev <alberto.lucchetti@nazaries.com>
2026-04-28 12:14:40 +0200

    filled details tabs with all informations

--- spa/src/app/features/user/components/user-details-tabs/user-details-tabs.ts
+++ spa/src/app/features/user/components/user-details-tabs/user-details-tabs.ts
@@ -1,15 +1,17 @@
-import { TitleCasePipe } from '@angular/common';
+import { DatePipe, TitleCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Department } from '@api/departments/DTOs/department.interface';
 import { APIUser } from '@api/users/DTOs/user.interace';
 
 @Component({
   selector: 'app-user-details-tabs',
-  imports: [MatTabsModule, TitleCasePipe],
+  imports: [MatTabsModule, TitleCasePipe, DatePipe],
   templateUrl: './user-details-tabs.html',
   styleUrl: './user-details-tabs.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserDetailsTabs { 
   user = input<APIUser>();
+  department = input<Department>();
 }
--- spa/src/app/features/user/pages/user-details-page/user-details-page.ts
+++ spa/src/app/features/user/pages/user-details-page/user-details-page.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { APIUser } from '@api/users/DTOs/user.interace';
 import { UserDetailsHeader } from "../../components/user-details-header/user-details-header";
 import { UserDetailsTabs } from "../../components/user-details-tabs/user-details-tabs";
+import { DepartmentsService } from '@app/features/departments/departments.service';
+import { Department } from '@api/departments/DTOs/department.interface';
 
 @Component({
   selector: 'app-user-details-page',
@@ -13,11 +15,21 @@ import { UserDetailsTabs } from "../../components/user-details-tabs/user-details
 })
 export class UserDetailsPage implements OnInit{ 
   route = inject(ActivatedRoute);
+  departmentsService = inject(DepartmentsService);
 
   userDetails = signal<APIUser>(null);
+  departmentDetails = signal<Department>(null);
 
   ngOnInit(): void {
     this.userDetails.set(this.route.snapshot.data['userDetails']);
     console.log(this.userDetails())
+    this.departmentsService.getDepartmentById(this.userDetails().department).subscribe({
+      next: ((res) => {
+        this.departmentDetails.set(res);
+      }),
+      error: ((err) => {
+        console.log(err);
+      })
+    })
   }
 }
